d1ef26cf3d701a890931d9d4401b78d75d12a0b4
victortoru <victortobaruelaruiz@elpuig.xeill.net>
2023-06-04 19:50:42 +0200

    Barra amb menu opcions

--- src/main/java/com/example/M03_Recu/HelloController.java
+++ src/main/java/com/example/M03_Recu/HelloController.java
@@ -1,16 +1,36 @@
 package com.example.M03_Recu;
 
 //Elements de la biblioteca per executar a JavaFX
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 
 import java.util.regex.Pattern;
 
 public class HelloController {
     @FXML
     private TextField fieldTelefon;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private MenuItem exitMenuItem;
+
+    @FXML
+    private MenuItem helpMenuItem;
+
+    @FXML
+    private void initialize() {
+        exitMenuItem.setOnAction(event -> sortirApp());
+        helpMenuItem.setOnAction(event -> helpOpcio());
+    }
 
     @FXML
     //Métode que executa l'Exepció en cas de format incorrecte
@@ -45,6 +65,22 @@ public class HelloController {
         alert.setTitle(title);
         alert.setHeaderText(null);
         alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private void sortirApp() {
+        Platform.exit();
+    }
+
+    private void helpOpcio() {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Ajuda");
+        alert.setHeaderText("Informació adicional");
+        alert.setContentText("Aquesta app valida el format d'un numero de telèfon.\n" +
+                "El numero de telèfon ha de tenir el format +XX-XXXXXXXXX.\n" +
+                "Per exemple: +34-123456789 es un numero de telèfon vèlid.\n\n" +
+                "Per comprovar el format del nº escrit, s'haurà d'introduïr al recuadre facilitat.\n" +
+                "Projecte realitzat per Víctor Tobaruela");
         alert.showAndWait();
     }
 }
